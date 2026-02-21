02154083066f358de76fc1ee795958e6c3f3b371
Ayan Adhikari <ayanadhi2022@gmail.com>
2026-02-21 09:12:36 +0800

    Feat: create buttons and add filler text.

--- script.js
+++ script.js
@@ -4,7 +4,19 @@ function pageLoaded()
 {
     console.log(`Page loaded`)
 }
-function generateRandomNumber(min, max) 
-{
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+//Functions
+  //Function to generate random number from min to max (including min and max)
+  function generateRandomNumber(min, max) 
+  {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+  function generateName()
+  {
+    text.textContent = `Name generation is currently unavailable.`
+  }
+
+//Assigning elements to variables
+const text = document.getElementById(`text`)
+const generateButton = document.getElementById(`generate`)
+//Event listeners
+generateButton.addEventListener(`click`, generateName)
